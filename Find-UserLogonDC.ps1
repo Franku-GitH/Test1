7c9705d9bf76dd4834fc6a7b1ad252fe7f1e57ca
Franku-GitH <frantisek.kurec@hotmail.com>
2025-11-11 12:04:16 +0100

    Add files via upload

---
+++ Find-UserLogonDC.ps1
@@ -0,0 +1,13 @@
+﻿$User = "uzivatel@domena.local"
+$DCs = @("DC1","DC2","DC3")  # Zde doplň názvy nebo IP adresy DC
+$OutputDC = "C:\Logs\RDP_DCs_$($User)_$(Get-Date -Format 'yyyyMMdd_HHmm').csv"
+
+$Results = foreach ($DC in $DCs) {
+    Get-WinEvent -ComputerName $DC -FilterHashtable @{LogName='Security'; ID=4768,4769,4771,4820,4821,4822,4823,4964} |
+    Where-Object { $_.Message -like "*$User*" } |
+    Select-Object TimeCreated, Id, @{Name='DomainController';Expression={$DC}}, Message
+}
+
+$Results | Sort-Object TimeCreated -Descending | Export-Csv -Path $OutputDC -NoTypeInformation
+
+Write-Host "Export dokončen: $OutputDC"XeXeT0p-202
---
+++ Find-UserLogonHost.ps1
@@ -0,0 +1,12 @@
+﻿# Find Logon events for a user connecting to a server
+
+$User = "uzivatel@domena.local"
+$OutputServer = "C:\Logs\RDP_Server_$($User)_$(Get-Date -Format 'yyyyMMdd_HHmm').csv"
+
+Get-WinEvent -FilterHashtable @{LogName='Security'; ID=4625,4624,4776} |
+Where-Object { $_.Message -like "*$User*" } |
+Select-Object TimeCreated, Id, @{Name='Computer';Expression={$_.MachineName}}, Message |
+Sort-Object TimeCreated -Descending |
+Export-Csv -Path $OutputServer -NoTypeInformation
+
+Write-Host "Export dokončen: $OutputServer"
