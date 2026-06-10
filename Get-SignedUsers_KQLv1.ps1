ab2a144c616fcd23564992891bca56d748dd148c
Franku-GitH <frantisek.kurec@hotmail.com>
2026-06-10 15:11:32 +0200

    Add files via upload

---
+++ Get-SignedUsers_KQLv1.ps1
@@ -0,0 +1,17 @@
+﻿let SuccessfulLogins =
+SigninLogs
+| where ResultType == 0
+| summarize FirstLogin = min(TimeGenerated) by UserId, UserPrincipalName;
+
+SuccessfulLogins
+| extend State = "Signed"
+| project UserPrincipalName, State, FirstLogin
+
+union (
+    SigninLogs
+    | summarize by UserId, UserPrincipalName
+    | join kind=anti (SuccessfulLogins) on UserId
+    | extend State = "NotSigned", FirstLogin = datetime(null)
+    | project UserPrincipalName, State, FirstLogin
+)
+| order by State asc
